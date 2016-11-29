dc471a01a5121bbe46ea6741286bade9a75ae471
Froilan Miranda <froilan.miranda@gmail.com>
2016-11-29 10:23:04 -0500

    update webhook

--- webhook.js
+++ webhook.js
@@ -2,7 +2,7 @@
 
 module.exports = (app)=> {
 	console.log("Webhook is running...");
-	app.post('./webhook',(req, res)=> {
+	app.post('/webhook',(req, res)=> {
 		console.log("Activate webhook");
 		console.log(req.body);
 		res.status(200).send();
